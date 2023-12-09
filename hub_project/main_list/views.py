bf3bc589e55b1066046fd0dc91a51c446815fb77
Pavel Senko <pashok00777@mail.ru>
2023-12-09 14:18:16 +0500

    Initial commit

---
+++ hub_project/main_list/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Hello')
+
+
+def person(request, name):
+    print(request.GET)
+    return HttpResponse(f'<h1>Данные</h1><p>{name}</p>')
+
+
+def product(request):
+    return HttpResponse('List products')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
